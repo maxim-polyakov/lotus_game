3e45596ce9f0e76972b3b97d6315652ea3501eb6
maxim-polyakov <maxim7012@gmail.com>
2026-09-18 01:26:49 +0300

    11

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('./game/PhaserApp', () => () => <div>Lotus Game</div>);
+
+test('renders the game application', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Lotus Game')).toBeInTheDocument();
 });
